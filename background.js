2f0b8b0299a2a48163e1781d5ff920c06619e375
dryabtse <tain.furrious@gmail.com>
2022-05-18 16:40:02 +1000

    Misc

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
   chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+    chrome.storage.sync.set({background: 'RUN'}, function() {
+      console.log("Background Service Worker started");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
