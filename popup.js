92455abafd8d4df4e5b6853e26b1fdfc1e7156aa
dryabtse <tain.furrious@gmail.com>
2023-07-04 10:55:36 +1000

    Enabled Customer Hub support

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 let ticketCopy = document.getElementById('ticketCopy');
 let nameCopy = document.getElementById('nameCopy');
 let manageTicket = document.getElementById('manageTicket');
-var nativeMessagingHost = 'com.villainsoftware.support_ex_caller';
+const nativeMessagingHost = 'com.villainsoftware.support_ex_caller';
+
+
 
 chrome.runtime.sendNativeMessage(nativeMessagingHost,
     { text: 'Hello'},
@@ -17,19 +19,40 @@ chrome.runtime.sendNativeMessage(nativeMessagingHost,
 var funcToInject = function() {
     var ticketNum = "";
     var contactName = "";
-    
-    var element = window.document.querySelector("body > div.desktop.container.forceStyle.oneOne.navexDesktopLayoutContainer.lafAppLayoutHost.forceAccess.tablet > div.viewport > section > div.navexWorkspaceManager > div > div.tabsetHeader.slds-context-bar.slds-context-bar--tabs.slds-no-print > div.slds-context-bar__secondary.navCenter.tabBarContainer > div > div > ul.tabBarItems.slds-grid > li.oneConsoleTabItem.tabItem.slds-context-bar__item.slds-context-bar__item_tab.slds-is-active.active.hasActions.hideAnimation.navexConsoleTabItem > a > span.title.slds-truncate");
-    if (element) {
-        ticketNum = element.innerHTML.split(":")[0];
-    } else ticketNum = "none found";
+    var element = null;
+
+    const getCurrentURL = function() {
+        return window.location.href
+      };
+      
+    // To get the current URL
+    const url = getCurrentURL();
+
+    if (url.includes("hub.corp.mongodb.com")) {
+        const link = new URL(url);
+        ticketNum = link.pathname.split("/")[2];
+    } else {
+        element = window.document.querySelector("body > div.desktop.container.forceStyle.oneOne.navexDesktopLayoutContainer.lafAppLayoutHost.forceAccess.tablet > div.viewport > section > div.navexWorkspaceManager > div > div.tabsetHeader.slds-context-bar.slds-context-bar--tabs.slds-no-print > div.slds-context-bar__secondary.navCenter.tabBarContainer > div > div > ul.tabBarItems.slds-grid > li.oneConsoleTabItem.tabItem.slds-context-bar__item.slds-context-bar__item_tab.slds-is-active.active.hasActions.hideAnimation.navexConsoleTabItem > a > span.title.slds-truncate");
+        if (element) {
+            ticketNum = element.innerHTML.split(":")[0];
+        } else ticketNum = "none found";
+    }
 
     console.log("Ticket found: " + ticketNum);
 
-  // Contact name retrieval is currently broken       
-    element = window.document.querySelector("#brandBand_2 > div > div > one-record-home-flexipage2 > forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___-cases_-l-w-c___-case___-v-i-e-w > forcegenerated-flexipage_cases_lwc_case__view_js > record_flexipage-record-page-decorator > div.record-page-decorator > records-record-layout-event-broker > slot > slot > flexipage-record-home-template-desktop2 > div > div.slds-col.slds-size_1-of-1.row.region-header > slot > flexipage-component2:nth-child(2) > slot > c-cc-highlights-panel > div.header-pin-wrapper > div > div.slds-page-header__row.slds-page-header__row_gutters > div > ul > li:nth-child(4) > div:nth-child(2) > a");
+    if (url.includes("hub.corp.mongodb.com")) {
+        element = window.document.querySelector("#case-header > div > div > div.case-header-bottom-div > div.case-header-fields-section > div:nth-child(2) > div > div:nth-child(3) > a > span");
+    } else {
+    // Contact name retrieval is currently broken       
+        element = window.document.querySelector("#brandBand_2 > div > div > one-record-home-flexipage2 > forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___-cases_-l-w-c___-case___-v-i-e-w > forcegenerated-flexipage_cases_lwc_case__view_js > record_flexipage-record-page-decorator > div.record-page-decorator > records-record-layout-event-broker > slot > slot > flexipage-record-home-template-desktop2 > div > div.slds-col.slds-size_1-of-1.row.region-header > slot > flexipage-component2:nth-child(2) > slot > c-cc-highlights-panel > div.header-pin-wrapper > div > div.slds-page-header__row.slds-page-header__row_gutters > div > ul > li:nth-child(4) > div:nth-child(2) > a");
+    }
+
     if (element) {
         contactName = element.innerHTML;
     } else contactName = "none found";
+
+    // contactName = url;
+
     return { 'ticketNum': ticketNum, 'contactName': contactName };
 };
 /* This line converts the above function to string
@@ -60,7 +83,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     });
 });
 
-var clipCopy = function(elementId) {
+const clipCopy = function(elementId) {
     const el = document.getElementById(elementId);
 
     if (document.body.createTextRange) {
@@ -94,7 +117,7 @@ nameCopy.onclick = function(element) {
 };
 
 manageTicket.onclick = function(element) {
-    var ticketNum = document.getElementById('ticket_found').innerHTML;
+    const ticketNum = document.getElementById('ticket_found').innerHTML;
     chrome.runtime.sendNativeMessage(nativeMessagingHost,
         { "ticketNum": ticketNum },
         function(response) {
@@ -110,7 +133,7 @@ clearSides.onclick = function(element) {
         { "clearSides": 1 },
         function(response) {
             console.log("Received " + JSON.stringify(response));
-            var resEl = document.createElement("p");
+            const resEl = document.createElement("p");
             resEl.innerHTML = JSON.stringify(response);
             document.body.appendChild(resEl);
         });
